366323adce57c06b9aa8ec4ca97eb04f6ad7fc4c
Ankur Srivastava <ankurlkw@gmail.com>
2015-11-17 13:47:35 +0530

    Basic skeleton for RunFragment

--- app/src/main/java/com/edocent/runtracker/RunFragment.java
+++ app/src/main/java/com/edocent/runtracker/RunFragment.java
@@ -6,6 +6,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -13,12 +15,27 @@ import android.view.ViewGroup;
  */
 public class RunFragment extends Fragment {
 
+    private Button mStartButton, mStopButton;
+    private TextView mStartedTextView, mLatitudeTextView,
+            mLongitudeTextView, mAltitudeTextView, mDurationTextView;
+
     public RunFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_run, container, false);
+
+        mStartedTextView = (TextView)view.findViewById(R.id.run_startedTextView);
+        mLatitudeTextView = (TextView)view.findViewById(R.id.run_latitudeTextView);
+        mLongitudeTextView = (TextView)view.findViewById(R.id.run_longitudeTextView);
+        mAltitudeTextView = (TextView)view.findViewById(R.id.run_altitudeTextView);
+        mDurationTextView = (TextView)view.findViewById(R.id.run_durationTextView);
+
+        mStartButton = (Button)view.findViewById(R.id.run_startButton);
+
+        mStopButton = (Button)view.findViewById(R.id.run_stopButton);
+
         return view;
     }
 }
