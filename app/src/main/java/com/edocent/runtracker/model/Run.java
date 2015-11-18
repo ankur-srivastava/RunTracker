2aa02d08f5e98a6e60071f31ab11a104fa27697f
Ankur Srivastava <ankurlkw@gmail.com>
2015-11-18 10:50:33 +0530

    Run Class added to store Run data

---
+++ app/src/main/java/com/edocent/runtracker/model/Run.java
@@ -0,0 +1,34 @@
+package com.edocent.runtracker.model;
+
+import java.util.Date;
+
+/**
+ * Created by Ankur on 11/18/2015.
+ */
+public class Run {
+
+    private Date mStartDate;
+
+    public Run() {
+        mStartDate = new Date();
+    }
+
+    public int getDurationSeconds(long endMillis) {
+        return (int)((endMillis - mStartDate.getTime()) / 1000);
+    }
+
+    public static String formatDuration(int durationSeconds) {
+        int seconds = durationSeconds % 60;
+        int minutes = ((durationSeconds - seconds) / 60) % 60;
+        int hours = (durationSeconds - (minutes * 60) - seconds) / 3600;
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    public Date getStartDate() {
+        return mStartDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        mStartDate = startDate;
+    }
+}
