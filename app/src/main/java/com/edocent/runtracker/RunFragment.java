545196b1017ef5d8e5afbb71302439380ccd423a
Ankur Srivastava <ankurlkw@gmail.com>
2015-11-18 10:43:55 +0530

    Location Receiver added, and Fragment updated with Start and Stop listeners

--- app/src/main/java/com/edocent/runtracker/RunFragment.java
+++ app/src/main/java/com/edocent/runtracker/RunFragment.java
@@ -9,17 +9,27 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.edocent.runtracker.utility.RunManager;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RunFragment extends Fragment {
 
+    RunManager runManager;
     private Button mStartButton, mStopButton;
     private TextView mStartedTextView, mLatitudeTextView,
             mLongitudeTextView, mAltitudeTextView, mDurationTextView;
 
     public RunFragment() { }
+
+    @Override
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setRetainInstance(true);
+        runManager = RunManager.get(getActivity());
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,9 +43,30 @@ public class RunFragment extends Fragment {
         mDurationTextView = (TextView)view.findViewById(R.id.run_durationTextView);
 
         mStartButton = (Button)view.findViewById(R.id.run_startButton);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                runManager.startLocationUpdates();
+                updateUI();
+            }
+        });
 
         mStopButton = (Button)view.findViewById(R.id.run_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                runManager.stopLocationUpdates();
+                updateUI();
+            }
+        });
 
+        updateUI();
         return view;
+    }
+
+    private void updateUI() {
+        boolean started = runManager.isTrackingRun();
+        mStartButton.setEnabled(!started);
+        mStopButton.setEnabled(started);
     }
 }
