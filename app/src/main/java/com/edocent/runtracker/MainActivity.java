ee6f853454e45e7692fee80179b68c13fa7e6758
Ankur Srivastava <ankurlkw@gmail.com>
2015-11-20 10:48:56 +0530

    Navigate to previous activity enabled

--- app/src/main/java/com/edocent/runtracker/MainActivity.java
+++ app/src/main/java/com/edocent/runtracker/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        getActionBar().setDisplayHomeAsUpEnabled(true);
         loadRunFragment();
     }
 
@@ -48,14 +50,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.addRun) {
             return true;
+        }else if(id == R.id.home){
+            //NavUtils.navigateUpFromSameTask(this);
+            //return true;
         }
 
         return super.onOptionsItemSelected(item);
